ef69e29e96b4ed79436fb20b4d9d26debed90691
RasmusEng <rasmus.engstrom@hotmail.com>
2024-11-18 20:01:05 +0100

    first iter

--- Lab2/solution1.py
+++ Lab2/solution1.py
@@ -1,32 +1,56 @@
 import random as r
+from itertools import accumulate
     
 def makeRandomArray(n: int, lowest =-10, highest =10):
     """ Makes an array n long with random ints between lowest and highest """
     return [r.randint(lowest, highest) for _ in range(n)]
 
-def maxSubSum(array, lower, higher, arrayLength):
-    return
+def maxSubCross(array, low, middle, high):
+    left_sum = float('-inf')
+    sum_left = 0
+    for i in range(middle, low - 1, -1):
+        sum_left += array[i]
+        if sum_left > left_sum:
+            left_sum = sum_left
 
+    # Max sum on the right of the midpoint
+    right_sum = float('-inf')
+    sum_right = 0
+    for i in range(middle + 1, high + 1):
+        sum_right += array[i]
+        if sum_right > right_sum:
+            right_sum = sum_right
+
+    return left_sum + right_sum
 
 def divideAndConquer(array, low, high):
     """ Assumes that array is 1 element or lager """
     if (low > high):
-        return -10000
+        return float('-inf')
     
-    if high == low:
+    if low == high:
         return array[low]
     
-    middle = high+1 // 2
+    middle = (low + high) // 2
     
-    left = divideAndConquer(array, low, middle-1)
+    left = divideAndConquer(array, low, middle)
     right = divideAndConquer(array, middle+1, high)
-    
-    return max(left, right)
+    cross = maxSubCross(array, low, middle, high)
+
+    return max(left, right, cross)
+
+def test(result, array):
+    if max(accumulate(array, lambda x, y: max(y, x + y))) == result:
+        return True
+    return False
+
 
 
 if __name__ == '__main__':
-    # Arr = makeRandomArray(10)
-    arr = [7, -5, 2, 6, -1]
+    arr = makeRandomArray(10)
+    #arr = [7, -5, 2, 6, -1]
     print(arr)
     # Send in index
-    print(divideAndConquer(arr, 0, len(arr)-1))
+    res = divideAndConquer(arr, 0, len(arr)-1)
+    print(res)
+    print(test(res, arr))
