4c4eb9629841722aac44f010e4785db177bb50cd
alijan-3 <alijan-3@student.ltu.se>
2024-11-19 02:40:38 +0100

    Sleep... I cannot

--- Lab2/testat.py
+++ Lab2/testat.py
@@ -1,34 +1,38 @@
 import random as r
 
 def mergeSort(array):
-    """ Assumes that array is 1 element or lager """
-    # Returns sorted 2 array
+    """ sorts array using mergeSort. Assumes that array is not empty """
+    
+    # Base case
     if len(array) == 1:
         return array
-    if len(array) == 2:
-        return [array[0], array[1]] if array[0] < array[1] else [array[1], array[0]]
     
+    # Variables
     middle = len(array) // 2
     left = mergeSort(array[:middle])
     right = mergeSort(array[middle:])
-    temp = []
+    tempArray = []
+
+    # Sorts arrays while merging them
     while len(right) and len(left) != 0:
         if left[0] < right[0]:
-            temp.append(left.pop(0))
+            tempArray.append(left.pop(0))
         else:
-            temp.append(right.pop(0))
+            tempArray.append(right.pop(0))
+
+    # Readds everything that's left over after sorting
     while len(left) != 0:
-        temp.append(left.pop(0))
+        tempArray.append(left.pop(0))
     while len(right) != 0:
-        temp.append(right.pop(0))
-    return temp
+        tempArray.append(right.pop(0))
 
-# Oläslighet            
+    # Owahhaha
+    return tempArray
 
-def makeRandomArray(n: int, lowest =-100000, highest =10000):
+def makeRandomArray(n: int, lowest =-100000, highest =100000):
     """ Makes an array n long with random ints between lowest and highest """
     return [r.randint(lowest, highest) for _ in range(n)]
 
 if __name__ == '__main__':
-    test = makeRandomArray(10)
+    test = makeRandomArray(100000)
     print(mergeSort(test))
