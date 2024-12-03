71bfaf0420e1aac0148df18201bf178f1d832520
Chicoo <byggvargen@gmail.com>
2024-12-03 15:48:46 +0100

    Start of lab3

---
+++ Lab3/Tests.py
@@ -0,0 +1,12 @@
+import BST
+import Node
+import time
+
+
+def main():
+    start_time = time.time()
+    
+    end_time = time.time()
+    print(end_time - start_time)
+if __name__ == '__main__':
+    main()
