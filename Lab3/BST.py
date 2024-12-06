ef3548f2e3da19bb464b16fc53b5d4181a6ba455
Chicoo <byggvargen@gmail.com>
2024-12-06 18:34:30 +0100

    Something

---
+++ Lab3/BST.py
@@ -0,0 +1,6 @@
+class BST:
+        def __init__(self):
+            head = None
+        
+        def insert(data):
+            
--- Lab3/Node.py
+++ Lab3/Node.py
@@ -2,5 +2,7 @@ class Node:
         def __init__(self, data):
             # Constructor for node, sets variables of node
             self.data = data
+            self.size = 1
             self.left = None
             self.right = None
+            self.parent = None
