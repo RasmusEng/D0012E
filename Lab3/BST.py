2742207d028991102fbd33a92f810fdc574f320a
alijan-3 <alijan-3@student.ltu.se>
2024-12-08 23:00:48 +0100

    Made a bad BST

--- Lab3/BST.py
+++ Lab3/BST.py
@@ -1,6 +1,51 @@
+import Node
+
 class BST:
         def __init__(self):
-            head = None
+            self.head = None
+
+        def isEmpty(self):
+            return self.head is None
         
-        def insert(data):
-            
+        def insert(self, data): # Inserts new element on correct spot
+            # If empty
+            if self.isEmpty():
+                self.head = Node.Node(data)
+                return
+
+            # Normal insert
+            previous = None
+            current = self.head
+            while current != None:
+
+                # Increase size if duplicates
+                if data == current.data:
+                    current.size += 1
+                    return
+                
+                # Traverse and find leaf
+                if data < current.data:
+                    previous = current
+                    current = previous.left
+                else:
+                    previous = current
+                    current = previous.right
+            current = Node.Node(data)
+
+            # Create links
+            current.parent = previous
+            if data < previous.data:
+                previous.left = current
+            else:
+                previous.right = current
+
+if __name__ == "__main__":
+    bst = BST()
+    bst.insert(10)
+    bst.insert(7)
+    bst.insert(4)
+    bst.insert(33)
+    bst.insert(9)
+    bst.insert(10)
+    bst.insert(10)
+    bst.insert(72)
