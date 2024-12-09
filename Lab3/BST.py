c3348ef2b1df50d1ed51d5cdfea10db0d336ef08
RasmusEng <rasmus.engstrom@hotmail.com>
2024-12-09 22:46:40 +0100

    yay2

--- Lab3/BST.py
+++ Lab3/BST.py
@@ -1,12 +1,13 @@
-import Node
+import Node 
 
 class BST:
         def __init__(self):
             self.head = None
+        
 
         def isEmpty(self):
             return self.head is None
-        
+
         def insert(self, data): # Inserts new element on correct spot
             # If empty
             if self.isEmpty():
@@ -17,12 +18,6 @@ class BST:
             previous = None
             current = self.head
             while current != None:
-
-                # Increase size if duplicates
-                if data == current.data:
-                    current.size += 1
-                    return
-                
                 # Traverse and find leaf
                 if data < current.data:
                     previous = current
@@ -30,19 +25,50 @@ class BST:
                 else:
                     previous = current
                     current = previous.right
+                previous.size += 1
             current = Node.Node(data)
-
-            # Create links
+            
+            # Create links and variables
+            current.depth = previous.depth + 1  # temp
             current.parent = previous
             if data < previous.data:
                 previous.left = current
             else:
                 previous.right = current
+            
+            
+            #self = self.balanceTree()
 
+        def inOrderWalk(self, leaf : Node):
+            list = []
+            
+            if(leaf.left is not None):
+                list += self.inOrderWalk(leaf.left)
+            list.append(leaf.data)
+            if(leaf.right is not None):
+                list += self.inOrderWalk(leaf.right)
+            
+            return list 
+        
+        def balanceTree(self):
+            list = self.inOrderWalk(self.head)
+            balanced = BST()
+            
+            mid = len(list)//2
+            print(list[mid])
+            balanced.insert(list[mid])
+            for i in range(mid, 0, -1):
+                balanced.insert(list[i])
+            for i in range(mid+1, len(list)-1):
+                balanced.insert(list[i])
+        
+            return balanced
+            
 if __name__ == "__main__":
     bst = BST()
     bst.insert(10)
     bst.insert(7)
+    bst.insert(4)
     bst.insert(4)
     bst.insert(33)
     bst.insert(9)
--- Lab3/Node.py
+++ Lab3/Node.py
@@ -2,6 +2,8 @@ class Node:
         def __init__(self, data):
             # Constructor for node, sets variables of node
             self.data = data
+            self.height = 0 # temp
+            self.depth = 0  # temp
             self.size = 1
             self.left = None
             self.right = None
