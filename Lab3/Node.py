c3d83fcd6e7171af2fa0178850174b0632e695ca
RasmusEng <rasmus.engstrom@hotmail.com>
2024-12-11 23:23:47 +0100

    yay

--- Lab3/Node.py
+++ Lab3/Node.py
@@ -2,9 +2,7 @@ class Node:
         def __init__(self, data):
             # Constructor for node, sets variables of node
             self.data = data
-            self.height = 0 # temp
-            self.depth = 0  # temp
-            self.size = 1
+            self.size = 0
             self.left = None
             self.right = None
             self.parent = None
